64072e737793ba8d6229a50d5d34b45851b5900d
irawan <pengetahuanburung@gmail.com>
2020-10-19 13:59:46 +0700

    add routing and firebase context

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC2vSebVOTiJzGqib7Jmt-syRIa0-bPJo0",
+    authDomain: "aplikasi-penjualan-3d61c.firebaseapp.com",
+    databaseURL: "https://aplikasi-penjualan-3d61c.firebaseio.com",
+    projectId: "aplikasi-penjualan-3d61c",
+    storageBucket: "aplikasi-penjualan-3d61c.appspot.com",
+    messagingSenderId: "260278440334",
+    appId: "1:260278440334:web:ac1220f9273738f69414df",
+    measurementId: "G-9HTN9QE354"
+};
+
+export default firebaseConfig;
